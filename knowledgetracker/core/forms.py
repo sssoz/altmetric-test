87d808710b40bdef128a675f1bff5455f8138c4a
Sophy <sophy.ouch@erudit.org>
2018-08-25 18:47:57 -0400

    Add choice field for timeframe

--- knowledgetracker/core/forms.py
+++ knowledgetracker/core/forms.py
@@ -2,14 +2,31 @@ from django import forms
 import requests
 
 
+TIMEFRAMES = (
+    ('1d', 'day'),
+    ('1w', 'week'),
+    ('1m', 'month'),
+    ('1y', 'year'),
+)
+
+
 class KnowledgeTrackerForm(forms.Form):
-    q = forms.CharField(max_length=250)
+    q = forms.CharField(
+        max_length=250,
+        label='What’s trending on the subject of',
+    )
+
+    timeframe = forms.ChoiceField(
+        label='in the last',
+        choices=TIMEFRAMES,
+    )
 
     def search(self):
         results = {}
         q = self.cleaned_data['q']
-        endpoint = 'http://api.altmetric.com/v1/citations/1d?q={query}'
-        url = endpoint.format(query=q)
+        timeframe = self.cleaned_data['timeframe']
+        endpoint = 'http://api.altmetric.com/v1/citations/{tf}?q={query}'
+        url = endpoint.format(tf=timeframe, query=q)
         response = requests.get(url)
 
         if response.status_code == 200:  # SUCCESS
