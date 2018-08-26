0b03e6158d9831fea43299f53c3255be82e9a102
Sophy <sophy.ouch@erudit.org>
2018-08-26 17:16:28 -0400

    Add pagination
    ...and do not escape journal titles

--- knowledgetracker/core/forms.py
+++ knowledgetracker/core/forms.py
@@ -21,12 +21,26 @@ class KnowledgeTrackerForm(forms.Form):
         choices=TIMEFRAMES,
     )
 
+    page = forms.IntegerField(
+        label='Pagination',
+        initial=1,
+        widget=forms.HiddenInput(),
+    )
+
     def search(self):
         results = {}
         q = self.cleaned_data['q']
         timeframe = self.cleaned_data['timeframe']
-        endpoint = 'http://api.altmetric.com/v1/citations/{tf}?q={query}'
-        url = endpoint.format(tf=timeframe, query=q)
+        page = self.cleaned_data['page']
+        endpoint = 'http://api.altmetric.com/v1/citations/{tf}?q={q}\
+            &num_results={num_results}&page={p}&order_by={order_by}'
+        url = endpoint.format(
+            tf=timeframe,
+            q=q,
+            num_results=24,
+            p=page,
+            order_by='at_score',
+        )
         response = requests.get(url)
 
         if response.status_code == 200:  # SUCCESS
@@ -38,9 +52,11 @@ class KnowledgeTrackerForm(forms.Form):
                 results['message'] = 'No entry found for <strong>%s</strong> \
                 in the last <strong>%s</strong>!' % (q, timeframe)
             elif response.status_code == 502:  # BAD GATEWAY
-                results['message'] = 'KnowledgeTracker is down for maintenance.'
+                results['message'] = 'KnowledgeTracker is down for \
+                maintenance.'
             elif response.status_code == 429:  # TOO MANY REQUESTS
                 results['message'] = 'Too many requests! Try again soon.'
             elif response.status_code == 403:  # FORBIDDEN
-                results['message'] = 'You are not authorized to make this request.'
+                results['message'] = 'You are not authorized to make this \
+                request.'
         return results
